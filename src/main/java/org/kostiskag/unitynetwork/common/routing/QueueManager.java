9a996c1edeb02e2107d9c9330a71fbc426f88705
kostis <kostiskag.gmail.com>
2019-08-12 16:59:48 +0300

    Queue manager works with priorityqueue 2

--- src/main/java/org/kostiskag/unitynetwork/common/routing/QueueManager.java
+++ src/main/java/org/kostiskag/unitynetwork/common/routing/QueueManager.java
@@ -1,6 +1,5 @@
 package org.kostiskag.unitynetwork.common.routing;
 
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.concurrent.atomic.AtomicBoolean;
